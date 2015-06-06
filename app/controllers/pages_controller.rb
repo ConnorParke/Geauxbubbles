83fc36f02c9f4a56bd63d7e5b9eefa18366e5696
kennethbeers <kbbeers08@yahoo.comgit config -lgit config --global user.email kbbeers08@yahoo.com>
2015-06-05 21:38:43 -0500

    controller and pages added

---
+++ app/controllers/pages_controller.rb
@@ -0,0 +1,22 @@
+class PagesController < ApplicationController
+  def home
+  end
+
+  def about
+  end
+
+  def services
+  end
+
+  def shop
+  end
+
+  def gallery
+  end
+
+  def contact
+  end
+
+  def faq
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,18 @@
 Rails.application.routes.draw do
+  get 'pages/home'
+
+  get 'pages/about'
+
+  get 'pages/services'
+
+  get 'pages/shop'
+
+  get 'pages/gallery'
+
+  get 'pages/contact'
+
+  get 'pages/faq'
+
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
